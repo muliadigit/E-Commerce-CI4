8bfbc3fc0576a9b88ab58f6f35bb52d304f8159c
muliadigit <adhymaros33@gmail.com>
2021-07-28 23:11:59 +0800

    first commit

---
+++ app/Controllers/Products.php
@@ -0,0 +1,41 @@
+<?php namespace App\Controllers;
+
+use App\Models\ProductModel;
+use App\Models\CategoryModel;
+use App\Models\BrandModel;
+use App\Models\ProductImageModel;
+
+class Products extends BaseController
+{
+    protected $productModel;
+    protected $categoryModel;
+    protected $brandModel;
+    protected $productImageModel;
+    protected $perPage = 12;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel();
+        $this->categoryModel = new CategoryModel();
+        $this->brandModel = new BrandModel();
+        $this->productImageModel = new ProductImageModel();  
+        
+        $categories = $this->categoryModel->getNestedCategories();
+
+        $this->data['categories'] = $categories;
+        $this->data['brands'] = $this->brandModel->findAll();
+    }
+
+    public function index()
+    {
+        $products = $this->productModel
+            ->select('products.*')
+            ->where('status', $this->productModel::ACTIVE)
+            ->where('products.parent_id IS NULL');
+        $this->data['products'] = $products->paginate($this->perPage, 'bootstrap');
+        $this->data['pager'] = $this->productModel->pager;
+
+        return view('themes/' . $this->data['currentTheme'] . '/products/index', $this->data);
+    }
+}
+
